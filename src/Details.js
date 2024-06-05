e016a97a7907f7cd348c9d73db21f841ddbb61c0
Shubham Mohod <141303490+shubhammohod1996@users.noreply.github.com>
2024-06-06 02:05:26 +0530

    Add files via upload

---
+++ src/Details.js
@@ -0,0 +1,178 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Shubham Mohod",
+  tagline: "I build things for web",
+  img: profile,
+  about: `I am a highly motivated Frontend Developer with a passion for continuous learning
+  and skill enhancement. My enthusiasm for web development drives effective
+  contributions to team success. With a solid technical foundation, I excel in dynamic
+  tech environments, embracing challenges for personal and professional growth. My
+  objective is to utilize expertise in crafting exceptional web experiences, addressing
+  complex issues, and staying ahead of evolving web development trends. Eager to
+  contribute meaningfully in a collaborative and innovative setting.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/shubhammohod/",
+  github: "https://github.com/shubhammohod1996",
+  twitter: "https://x.com/Shubham39869275",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Frontend Web Developer",
+    Company: `Skill Mate`,
+    Location: "Tamil Nadu",
+    Type: "Full Time",
+    Duration: "Present",
+  },
+  {
+    Position: "React JS Developer",
+    Company: `Aariya Tech Pune`,
+    Location: "Pune",
+    Type: "Internship",
+    Duration: "Feb 2023 - July 2023",
+  },
+  {
+    Position: "Web Developer",
+    Company: `Code Soft`,
+    Location: "West Bengal",
+    Type: "Internship",
+    Duration: "Aug 2023 - Aug 2023",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Frontend Development",
+    Company: "Udemy, YouTube, Google, Medium",
+    Location: "Online",
+    Type: "Full Time",
+    Duration: "Jan 2022 - Present",
+  },
+  {
+    Position: "Master Of Commerce",
+    Company: `G.S.Tompe Art Commerce And Science College.`,
+    Location: "Amravati",
+    Type: "Full Time",
+    Duration: " June 2018 - Dec 2019",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Multicard Design In React",
+    image: projectImage1,
+    description: `Responsible for Developing React Project using npx create-react-app environment with React-router Tech Stack used: ReactJS, JavaScript, CSS, react-router-dom, Axios.Info-Cards with API  !!!`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/shubhammohod1996",
+  },
+  {
+    title: "Registration Page",
+    image: projectImage2,
+    description: `Responsible For Developing a React Project Using npx create-react-app Environment with react-router. Responsible For Adding Features Like Register User, Message when Register successfully.!!!`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/shubhammohod1996",
+  },
+  {
+    title: "Login Page",
+    image: projectImage3,
+    description: ` Responsible For Developing a project with pure HTML, CSS.  No any CSS framework like Bootstrap, Material UI.!!!`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/shubhammohod1996",
+  },
+  {
+    title: "Calculator Application",
+    image: projectImage4,
+    description: `Created a web-based calculator using HTML, CSS, and JavaScript.
+    Enabled basic arithmetic operations and ensured accurate calculations.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/shubhammohod1996",
+  },
+  {
+    title: "Portfolio Website",
+    image: projectImage5,
+    description: `Developed a personal portfolio website using HTML, CSS, and JavaScript.
+    Highlighted projects, skills, and contact details for professional presentation.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/shubhammohod1996",
+  },
+  {
+    title: "Friends Card Application",
+    image: projectImage6,
+    description: `Develop and maintain a responsive web application using React JS with React hooks .
+    Implementing user interface components using HTML, CSS, and JavaScript.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/shubhammohod1996",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  Developer: "Shubham Mohod",
+  email: "shubhammohod1234@gmail.com",
+  phone: "+91 9860 5965 19",
+};
